882ab58226ec4416d9bfd736b4142e05289ae435
Khaled Yakdan <yakdan@code-intelligence.com>
2025-07-25 10:47:47 +0200

    Use WithLength annotation in the fuzz test

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -2,12 +2,15 @@ package org.example;
 
 public record Person(String firstName, String lastName, int age) {
     public Person {
-        if (age < 0) {
-            throw new IllegalArgumentException("Age cannot be negative");
-        }
     }
 
     public Person(String name, int age) {
         this(name.split("", 1)[0], name.split("", 1)[1], age);
+    }
+
+    public void run(byte[] data){
+        assert data != null : "Data cannot be null";
+        assert data.length > 0 : "Data length must be greater than zero";
+        assert data.length <= 10 : "Data length must be less than or equal to 10";
     }
 }
--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import com.code_intelligence.jazzer.junit.FuzzTest;
+import com.code_intelligence.jazzer.mutation.annotation.NotNull;
+import com.code_intelligence.jazzer.mutation.annotation.WithLength;
 
 class PersonTest {
 
     @FuzzTest(maxDuration = "1m")
-    void fuzzPerson(String firstName, String lastName, int age) {
-        new Person(firstName, lastName, age);
+    void fuzzPerson(String firstName, String lastName, int age, byte @NotNull @WithLength(min = 1, max = 10)[] data) {
+        Person p = new Person(firstName, lastName, age);
+        p.run(data);
     }
 }
